860158371395f729699e412de1e9bae421361911
Shlomo Brill <2153068@students.wits.ac.za>
2021-05-09 17:03:31 +0200

    5

--- change.js
+++ change.js
@@ -28,6 +28,7 @@ function getChange (payable, paid) {
   return change;
 };
 
+
 /* The code block below ONLY Applies to Node.js - This Demonstrates
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
 /* istanbul ignore next */
